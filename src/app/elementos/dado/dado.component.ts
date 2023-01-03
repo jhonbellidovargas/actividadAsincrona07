e5f1e94fcf5719867858be6b79781056eb0add2d
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-01-03 14:31:42 -0500

    tarea07

---
+++ src/app/elementos/dado/dado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent {
+
+}
